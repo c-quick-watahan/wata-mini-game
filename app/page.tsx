eb59ce870373e0e12c7ed94f65e88102629ae536
Carlo Quick <c_quick@watahan.co.jp>
2025-02-07 12:13:29 +0900

    authentication bug fixed, and is now only making one user. I am also adding a new user to the user collection when a new authentication happends.

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,41 @@
 "use client";
-import { useEffect, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth, signInAnonymously } from "@/lib/firebase/firebase";
+import { auth, signInAnonymously, db } from "@/lib/firebase/firebase";
+import { collection, addDoc } from "firebase/firestore";
 import CareerWrapper from "./ui/CareerWrapper";
 import Spinner from "./ui/Spinner";
+import { UserCredential } from "firebase/auth";
+
+async function addStuff(userCredential: UserCredential) {
+  console.log("User", userCredential.user);
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      id: userCredential.user.uid,
+      gamesComplete: [],
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
 
 export default function Home() {
   const [user, loading] = useAuthState(auth);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (!user && !loading) {
-      signInAnonymously(auth)
-        .then((userCredential) => {
-          console.log("User", userCredential.user);
-        })
-        .catch((error) => {
-          console.error("Error signing in:", error);
-          setError(error);
-        });
+    if (loading) {
+      // console.log("loading....");
+      return;
+    } else if (!user) {
+      signInAnonymously(auth).then((userCredential) => {
+        addStuff(userCredential);
+      });
+    } else {
+      return;
     }
-  }, [user, loading]);
+  }, [loading]);
   if (error) return <p>Error: {error}</p>;
   return <div>{user ? <CareerWrapper /> : <Spinner />}</div>;
 }
--- app/ui/CareerWrapper.tsx
+++ app/ui/CareerWrapper.tsx
@@ -1,5 +1,9 @@
 import CareerCard from "./CareerCard";
 import { careers } from "../interfaces/Game";
+import { auth, signInAnonymously, db } from "@/lib/firebase/firebase";
+import { collection, addDoc, getDocs, setDoc, doc } from "firebase/firestore";
+
+const user = auth.currentUser;
 
 export default function CareerWrapper() {
   return (
@@ -10,6 +14,14 @@ export default function CareerWrapper() {
       {careers.map((career) => (
         <CareerCard key={career.careerId} careerId={career.careerId} />
       ))}
+      {/* <button
+        onClick={() => {
+          getUserDocumentNumber();
+        }}
+        className="btn bg-white btn-ghost text-[#0066A5] text-lg"
+      >
+        Add Game
+      </button> */}
     </div>
   );
 }
