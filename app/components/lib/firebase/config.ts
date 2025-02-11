04ec6a5e991cdc741b24c1de8f52a39b59b28999
Carlo Quick <c_quick@watahan.co.jp>
2025-02-11 15:51:26 +0900

    Fixes for deployment

--- app/components/lib/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyCgHOlaaEMO7FMhpYcs9bXW1N4TL4JGiWA",
-//   authDomain: "watahan-mini-game.firebaseapp.com",
-//   projectId: "watahan-mini-game",
-//   storageBucket: "watahan-mini-game.appspot.com",
-//   messagingSenderId: "630524334920",
-//   appId: "1:630524334920:web:c4d6fe72ed7bba922c997d",
-//   measurementId: "G-Y6YEP04K7C",
-// };
